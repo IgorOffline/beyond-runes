2053aabb86e0e9678641a05afc402e0a6339a7f4
Igor Durbek <igor@igordurbek.com>
2025-06-15 10:46:53 +0200

    Svelte, initial practice

--- first/src/routes/+page.svelte
+++ first/src/routes/+page.svelte
@@ -1,9 +1,26 @@
 <script lang="ts">
 	import Nested from '$lib/Nested.svelte';
   let name = 'Svelte';
+	let count = $state(99)
+	function increment() {
+		count += 1;
+	}
+	let numbers = $state([1, 2, 3, 4]);
+	function addNumber() {
+		//numbers[numbers.length] = numbers.length + 1;
+		numbers.push(numbers.length + 1);
+	}
+	let total = $derived(numbers.reduce((t, n) => t + n, 0));
 </script>
 
 <h1>Hello {name.toUpperCase()}!</h1>
+<button onclick="{increment}">
+	Clicked {count}
+</button>
+<button onclick="{addNumber}">
+	Clicked {numbers}
+</button>
+<p>{numbers.join(' + ')} = {total}</p>
 <Nested />
 <Nested />
 <Nested />
